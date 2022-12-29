8917d8ace1cca2be132701ba38579010adb7dfbc
maxthestranger <thestrangermax@gmail.com>
2022-12-30 00:02:13 +0300

    add project setup

---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div className="bg-red-100 p-5">
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+  )
+}
+
+export default Login
