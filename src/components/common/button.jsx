837a1825ed0a55acc0b2d3e6e532d0a94a21ddac
maxthestranger <thestrangermax@gmail.com>
2023-01-12 11:33:08 +0300

    modify common components

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -1,6 +1,5 @@
-const Button = ({ children, type, ...props }) => (
+const Button = ({ children, ...props }) => (
   <button
-    type={type}
     className="bg-emerald-500 text-base font-bold py-3.5 w-full text-white rounded-md transition-all duration-100 ease-in hover:bg-emerald-600 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
     {...props}
   >
--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,11 +1,5 @@
-const Input = ({ type, name, id, placeholder, onchange, value, ...props }) => (
+const Input = ({ ...props }) => (
   <input
-    type={type}
-    name={name}
-    id={id}
-    placeholder={placeholder}
-    value={value}
-    onChange={onchange}
     {...props}
     className="w-full border border-gray-300 rounded-[6px] px-5 py-4 focus:outline-none focus:border-primary placeholder:text-place placeholder:text-sm placeholder:font-bold"
   />
